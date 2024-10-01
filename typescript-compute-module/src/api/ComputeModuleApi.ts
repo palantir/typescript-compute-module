8786407377b8c8d59f7c397fd35463105bc1c63e
Christopher Jeganathan <4092952+chrisjeg@users.noreply.github.com>
2024-10-01 18:26:11 +0100

    fix post result

--- typescript-compute-module/src/api/ComputeModuleApi.ts
+++ typescript-compute-module/src/api/ComputeModuleApi.ts
@@ -40,7 +40,7 @@ export class ComputeModuleApi {
   public postResult = <ResponseType>(jobId: string, response: ResponseType) =>
     this.axiosInstance.post(
       this.connectionInformation.postResultUri + "/" + jobId,
-      response,
+      typeof response === "number" ? response.toString() : response,
       {
         headers: {
           "Content-Type": "application/octet-stream",
